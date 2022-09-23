abb63d6dee4119a41e100798fa8e2c2e0ef6e263
Bubotka <108426053+Bubotka@users.noreply.github.com>
2022-09-23 20:16:16 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+new WOW().init();
+ new Swiper('.swiper', {
+  pagination: {
+    el: '.project-pagination',
+    bulletClass:"project-bullet",
+    bulletActiveClass:"project-bullet-active",
+    clickable:true
+  },
+});
